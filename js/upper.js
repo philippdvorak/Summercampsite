c160510ec57d442d3a5ebdef76b7fa4db4735b93
DDDisch <philipp.dvorak@sz-ybbs.ac.at>
2019-03-13 14:15:29 +0100

    Added More Cards and JS Function

---
+++ js/upper.js
@@ -0,0 +1,7 @@
+function createCardRow(mainID) {
+  $(mainID).append("<div class='row'></div>");
+}
+
+function createCardCol(mainID, faICON, title, content) {
+  $(mainID).find('.row').append("<div class='card' style='width: 14vw;margin: 10px;'><i class='" + faICON +  " fa-10x bg-dark'></i><div class='card-body'><h5 class='card-title'>Fotografie</h5><p class='card-text'>Wolltest du schon immer Professionel Bilderbearbeiten.</p></div></div>");
+}
