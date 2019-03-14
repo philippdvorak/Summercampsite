fff695bb78dc3893825b366c212e336aa4313006
DDDisch <philipp.dvorka@sz-ybbs.ac.at>
2019-03-14 13:44:57 +0100

    Deleted Background of FA Icons and added Padding to Card to fit Size

--- js/upper.js
+++ js/upper.js
@@ -3,5 +3,5 @@ function createCardRow(mainID) {
 }
 
 function createCardCol(mainID, faICON, title, content) {
-  $(mainID).find('.row').append("<div class='card' style='width: 14vw;margin: 10px;'><i class='" + faICON +  " bg-dark'></i><div class='card-body'><h5 class='card-title'>Fotografie</h5><p class='card-text'>Wolltest du schon immer Professionel Bilderbearbeiten.</p></div></div>");
+  $(mainID).find('.row').append("<div class='card' style='width: 14vw;margin: 10px;'><i class='" + faICON +  "'></i><div class='card-body'><h5 class='card-title'>Fotografie</h5><p class='card-text'>Wolltest du schon immer Professionel Bilderbearbeiten.</p></div></div>");
 }
