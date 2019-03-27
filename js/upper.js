0ff9f9789f685484ae4fe22bf8513014a56d9163
DDDisch <philipp.dvorak@sz-ybbs.ac.at>
2019-03-27 14:00:41 +0100

    Made Cards Responsive

--- js/upper.js
+++ js/upper.js
@@ -3,5 +3,5 @@ function createCardRow(mainID) {
 }
 
 function createCardCol(mainID, faICON, title, content) {
-  $(mainID).find('.row').append("<div class='card' id='cards' style='margin-left: 10px; margin-right: 10px; width: 15vw;'><i class='" + faICON +  "'></i><div class='card-body'><h5 class='card-title'>" + title + "</h5><p class='card-text'>" + content + "</p></div></div>");
+  $(mainID).find('.row').append("<div class='card' id='cards'><i class='" + faICON +  "'></i><div class='card-body'><h5 class='card-title'>" + title + "</h5><p class='card-text'>" + content + "</p></div></div>");
 }
